b59e413d0897ed592386f0c86485949f78de470a
Oscar Monroy <oscarmonroy657@gmail.com>
2023-10-19 07:07:07 -0600

    fea(main, Movie, Series): Add the models and a basic ui

---
+++ Movie.py
@@ -0,0 +1,15 @@
+class Movie:
+    _movies = []
+
+    def save(self, movie_properties: list) -> None:
+        movie_ob = {
+            "id": len(self._movies),
+            "name": movie_properties[0],
+            "duration": movie_properties[2],
+            "rating": movie_properties[3],
+            "genre": movie_properties[4]
+        }
+        self._movies.append(movie_ob)
+
+    def get_movies(self) -> list:
+        return self._movies
---
+++ Series.py
@@ -0,0 +1,16 @@
+class Series:
+    _series = []
+
+    def save(self, series_properties: list[str]) -> None:
+        series_obj = {
+            "id": len(self._series),
+            "name": series_properties[0],
+            "seasons": series_properties[2],
+            "rating": series_properties[3],
+            "genre": series_properties[4],
+            "release_date": series_properties[5]
+        }
+        self._series.append(series_obj)
+
+    def get_series(self):
+        return self._series
